c62b60bb13634c7296863a2783ae7473f01de928
MtnManJake <jharless7791@gmail.com>
2025-04-24 22:31:09 -0500

    D: Added more books and changed code to accomodate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys
 from stats import get_book_text, get_num_words, get_char_count, sort_char_counts
-
-path = "books/frankenstein.txt"
+if len(sys.argv) !=2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+path = sys.argv[1]
 text = get_book_text(path)
 word_count = get_num_words(text)
 char_count = get_char_count(text)
