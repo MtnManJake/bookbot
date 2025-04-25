bc6dd41a3d0c73aca6634af8c88e2c5e0558e171
MtnManJake <jharless7791@gmail.com>
2025-04-24 21:52:59 -0500

    B: Added character counter in place of word counter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from stats import get_book_text, get_num_words
+from stats import get_book_text, get_num_words, get_char_count
 
 path = "books/frankenstein.txt"
 text = get_book_text(path)
 word_count = get_num_words(text)
-
-print(f"{word_count} words found in the document")
+char_count = get_char_count(text)
+print(char_count)
--- stats.py
+++ stats.py
@@ -5,4 +5,14 @@ def get_book_text(path):
 def get_num_words(text):
     return len(text.split())
 
+def get_char_count(text):
+    char_count = {}
+    for letter in text:
+        letter = letter.lower()
+        if letter in char_count:
+            char_count[letter] += 1
+        else:
+            char_count[letter] = 1
+    return char_count
+
 
