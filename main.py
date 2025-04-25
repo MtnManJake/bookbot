669981bab3c79a8f27689ff81a0acc374e20a91e
MtnManJake <jharless7791@gmail.com>
2025-04-24 22:17:49 -0500

    C: Added fancy little breakdown printout

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from stats import get_book_text, get_num_words, get_char_count
+from stats import get_book_text, get_num_words, get_char_count, sort_char_counts
 
 path = "books/frankenstein.txt"
 text = get_book_text(path)
 word_count = get_num_words(text)
 char_count = get_char_count(text)
-print(char_count)
+sorted_chars = sort_char_counts(char_count)
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {path}")
+print("----------- Word Count ----------")
+print(f"Found {word_count} total words")
+print("--------- Character Count -------")
+for item in sorted_chars:
+    print(f"{item['char']}: {item['num']}")
--- stats.py
+++ stats.py
@@ -14,5 +14,17 @@ def get_char_count(text):
         else:
             char_count[letter] = 1
     return char_count
+    
+def sort_char_counts(char_count):
+    sorted_char_count = []
+    for char, count in char_count.items():
+        if char.isalpha():
+            entry = {'char':char, 'num': count}
+            sorted_char_count.append(entry)
+            sorted_char_count.sort(key=lambda x: x['num'], reverse=True)
+    return sorted_char_count
+            
 
 
+
+    
